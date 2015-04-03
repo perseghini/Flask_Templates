be75bf83e0207eed7028d6eb851e0d1ac15e5f21
Ludovic Perseghini <perseghini@gmail.com>
2015-04-03 10:50:49 -0700

    PostgreSQL Connection.

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ DEBUG = True
 SECRET_KEY = 'Use a better secret key than this!'
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,
-                                                      'flask_templates.db')
+SQLALCHEMY_DATABASE_URI = 'postgresql://USER:PASSWORD@localhost/' \
+                          'flask_templates'
